7a622b6353a5298c074a89bdea4fe51800e58ee1
“Lucas <coutinho0604@gmail.com>
2020-03-31 19:56:57 -0300

    Adicionado alguns testes de validação.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); //Importando o módulo Express pra dentro da variável express
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express(); //Armazena a aplicação
@@ -7,8 +8,7 @@ const app = express(); //Armazena a aplicação
 app.use(cors());
 app.use(express.json()); //Faz o express entender que as requisições estão no formato JSON
 app.use(routes);
+app.use(errors());
 
-app.listen(3333); //localhost:3333
-
-
+module.exports = app;
 
